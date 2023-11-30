2cbbf605890d588a72442be616271d6fb55e818b
Gaurav Mall 26 <122550605+Samrathevil26@users.noreply.github.com>
2023-11-30 12:58:06 +0530

    successfull

---
+++ prectice/src/test/java/Runner/TestRunner.java
@@ -0,0 +1,33 @@
+package Runner;
+
+import org.junit.runner.RunWith;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features = "C:\\Users\\91630\\newWorkSpace\\prectice\\src\\test\\java\\feature\\demo.feature", glue = { "Steps" }, plugin = {
+		"html:target/Cucumberreport.html", "pretty",
+		"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:", "timeline:test-output-thread/" })
+
+public class TestRunner {
+
+	// A cucumber runner file is used to build a communication between your feature
+	// files and step defination file
+	// Whenever we have to run multiple features or multiple steps, then we use
+	// Cucumber runner file.
+	// Whenever have to generate reports , we will use cucumber runner file
+
+	/*
+	 * - cucumber report - Extent report - Cucumber reports in PDF format - HTML
+	 * Reports - TestNG Reports
+	 * 
+	 * Various Cucumber options or properties :
+	 * 
+	 * features : comma separted paths to feature file. dryRun=true : It will check
+	 * if every steps has an step defination method implemented or not glue : comma
+	 * separated package names of step def. file plugin = pretty : Tags
+	 * 
+	 */
+
+}
---
+++ prectice/test-output-thread/report.js
@@ -0,0 +1,4 @@
+$(document).ready(function() {
+CucumberHTML.timelineItems.pushArray([{"id":"41ac5843-c89f-4c09-b8b3-1e020f035b19","feature":"Create account on wiki page","scenario":"User creates account on wiki page using input from cucumber data table","start":1701329066328,"group":1,"content":"","tags":"","end":1701329079191,"className":"passed"}]);
+CucumberHTML.timelineGroups.pushArray([{"id":1,"content":"Thread[main,5,main]"}]);
+});
